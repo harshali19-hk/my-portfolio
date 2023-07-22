466fc49ec12e0641b82d2b8c1eef4fbd0242cbe9
harshali19-hk <kaleharshali1998@gmail.com>
2023-07-22 18:54:32 +0530

    modified

--- src/App.js
+++ src/App.js
@@ -5,31 +5,20 @@ import ContactSection from './Components/ContactSection';
 import HomeSection from './Components/HomeSection';
 import ProjectSection from './Components/ProjectSection';
 import TechStack from './Components/TechStack';
-import { BrowserRouter} from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 
 function App() {
   return (
     <>
-    {/*  <BrowserRouter>
-       
-      
-       <Routes>
-       <Route index element={<HomeSection />} />
-       <Route path='about' element={<AboutSection />} />
-       <Route path='projects' element={<ProjectSection />} />
-       <Route path='contact' element={<ContactSection />} />
-       </Routes>
-       <TechStack></TechStack>
-    </BrowserRouter> */}
 
-    <BrowserRouter>
-    <HomeSection></HomeSection>
-    <TechStack></TechStack>
-    <AboutSection></AboutSection>
-    <ProjectSection></ProjectSection>
-    <ContactSection></ContactSection>
-    </BrowserRouter>
+      <BrowserRouter>
+        <HomeSection></HomeSection>
+        <TechStack></TechStack>
+        <AboutSection></AboutSection>
+        <ProjectSection></ProjectSection>
+        <ContactSection></ContactSection>
+      </BrowserRouter>
 
     </>
   );
